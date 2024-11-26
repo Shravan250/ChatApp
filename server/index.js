6817a4b10208dfe36f4c1030ba10a3d3800644da
Shravan250 <shravanbobade25@gmail.com>
2024-11-26 18:05:28 +0530

    ready

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5003;
 
 require("dotenv").config();
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "public/index.html"));
+});
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
